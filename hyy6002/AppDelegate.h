5a4dc867eceac16d2ef30f28811fa5e680e598da
HYY666 <mac@huangyanyun.local>
2016-06-24 16:23:39 +0800

    0.0

---
+++ hyy6002/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hyy6002
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 hyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hyy6002/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hyy6002
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 hyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
